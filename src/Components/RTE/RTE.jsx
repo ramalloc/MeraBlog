c4bf6fe4c08ff89ee52e64b87cdc124e9e613b79
ramalloc <95184750+ramalloc@users.noreply.github.com>
2024-06-15 11:30:10 +0530

    Update RTE.jsx

--- src/Components/RTE/RTE.jsx
+++ src/Components/RTE/RTE.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 // We have to import Editor from tniy-mce
 import { Editor } from '@tinymce/tinymce-react'
-import envImporter from '../../Config/envImporter.js'
+// import envImporter from '../../Config/envImporter.js'
 
 {/*We will use this component in many other components. So we have to get the reference of it to use it.
 Here we are using Controller*/}
@@ -31,7 +31,7 @@ const RTE = ({ name, control, label, defaultvalue = "" }) => {
         render={({ field: { onChange } }) => (
           // Now here will pass the component or element we want render
           <Editor
-            apiKey={envImporter.tinyApi}
+{/*             apiKey={envImporter.tinyApi} */}
             initialValue={defaultvalue}
             init={{
               initialValue: defaultvalue,
